414528e70f6ce5e0df760ea8b62ce08f75235c47
Graham <ghill@unb.ca>
2020-02-01 16:56:54 -0400

    Menus - Added some swaying to add depth to menu

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    private RectTransform obj;
+    private Vector3 start;
+    private Vector3 target;
+    public float speed;
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = GetComponent<RectTransform>();
+        start = obj.transform.position;
+        target = new Vector3(start.x + 89,start.y, start.z);
+        speed = 0.25f;
+        distance = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       obj.transform.position =  Vector3.Lerp(start, target, distance);
+       distance += speed * Time.deltaTime;
+       if(distance >= 1){
+           distance = 0.0f;
+           Vector3 temp = start;
+           start = target;
+           target = temp;
+       }
+       //Debug.Log(target);
+    }
+}
