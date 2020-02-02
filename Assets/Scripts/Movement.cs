bf2e4d31b13dd6c291e9eb46f4f5407fc1da6a97
Edgar Cobos <edgarcobos00@gmail.com>
2020-02-02 17:04:58 -0400

    Add particle collisions
    - could not change the color of the balloons

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,6 +10,8 @@ public class Movement : MonoBehaviour
     public AudioClip[] sounds;
 	private Rigidbody2D characterBody;
 	private float ScreenWidth;
+    private Animator anim;
+    //private GameController controllerScript = (GameController) GameController.GetComponent("GameController");
 
     // Start is called before the first frame update
     void Start() {
@@ -17,6 +19,8 @@ public class Movement : MonoBehaviour
         ScreenWidth = Screen.width;
         characterBody = character.GetComponent<Rigidbody2D>();
         speed = 1.0f;
+        anim = GetComponent<Animator>();
+        anim.Play("Balloon");
     }
 
     // Update is called once per frame
@@ -53,5 +57,26 @@ public class Movement : MonoBehaviour
         else {
             characterBody.velocity = new Vector2(0f, characterBody.velocity.y);
         }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider) {
+        if(collider.gameObject.tag == "Obstacle") {
+            Destroy(gameObject);
+            return;
+        }
+        if(collider.gameObject.tag == "White") {
+            anim.Play("Balloon_White_1");
+        }
+        if(collider.gameObject.tag == "Red") {
+            anim.Play("Balloon");
+        }
+        if(collider.gameObject.tag == "Yellow") {
+            anim.Play("Balloon_Yellow_1");
+        }
+        if(collider.gameObject.tag == "Blue") {
+            anim.Play("Balloon_Blue_1");
+        }
+        //controllerScript.particlePoints++;
+        Destroy(collider.gameObject);
     }
 }
